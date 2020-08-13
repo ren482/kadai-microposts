f74f412fc9bf7afeb08ac6902afba85a36fd6d77
ren.shibuya <ren.hibuyas@gmail.com>
2020-08-13 12:24:38 +0000

    user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
   root to: "toppages#index"
+  
+  
+  #resources 内には、get "users_new", to: "users#new" が記述されている(newアクションがルーティングに追加されている)
+  #のに、get "singup", to: "users#name"としたのは、usersコントローラのnewアクションへのURLをsingupに変更したかったから
+  get "signup", to: "users#new"
+  
+  resources :users, only: [:index, :show, :create]
+  
 end
